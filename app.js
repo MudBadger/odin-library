57d0707d63b3d03312a715ffe6689dcf36d98c1d
Gaëtan Charolle <gaetan.charolle@gmail.com>
2025-02-16 03:54:29 +0100

    style modal

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dialogBox = document.querySelector("#dialogBox");
 const closeModalBtn = document.querySelector("#closeModalBtn");
 const submitBookBtn = document.querySelector("#submitBookBtn");
 const bookForm = document.querySelector("#bookForm");
+
 addBookBtn.addEventListener("click", () => {
     dialogBox.showModal();
 });
@@ -41,17 +42,31 @@ addBookToLibrary("Ranma 1/2", "Takahashi", "Manga");
 function displayBook(book) {
     const bookCards = document.createElement("div");
     bookCards.className = "bookCards";
+
+    const closeCards = document.createElement("div");
+    closeCards.className = "closeCards";
+    closeCards.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+
     const title = document.createElement("p");
     title.className = "bookInfos";
+
     const author = document.createElement("p");
     author.className = "bookInfos";
+
     const genre = document.createElement("p");
     genre.className = "bookInfos";
+
     title.textContent = "Title : " + book.title;
     author.textContent = "Author : " + book.author;
     genre.textContent = "Genre : " + book.genre;
+
+    bookCards.appendChild(closeCards);
     bookCards.appendChild(title);
     bookCards.appendChild(author);
     bookCards.appendChild(genre);
     booksContainer.appendChild(bookCards);
+
+    closeCards.addEventListener("click", () => {
+        bookCards.remove();
+    });
 }
