742db7148344cf2d10a54b4e65f2e4876ac0e420
Gaëtan Charolle <gaetan.charolle@gmail.com>
2025-02-16 19:29:31 +0100

    ajout bouton changement read status

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const closeModalBtn = document.querySelector("#closeModalBtn");
 const submitBookBtn = document.querySelector("#submitBookBtn");
 const bookForm = document.querySelector("#bookForm");
 
+const bookOne = new Book("The Hobbit", "Tolkien", "Fantasy", "Yes");
+const bookTwo = new Book("Dune", "Herbert", "SF", "No");
+const bookThree = new Book("Ranma 1/2", "Takahashi", "Manga", "Yes");
+
 addBookBtn.addEventListener("click", () => {
     dialogBox.showModal();
 });
@@ -18,26 +22,43 @@ submitBookBtn.addEventListener("click", (event) => {
     event.preventDefault();
     const bookInputs = [...bookForm.elements];
     const bookArray = bookInputs.map((input) => input.value);
-    const newBook = new Book(bookArray[0], bookArray[1], bookArray[2]);
-    addBookToLibrary(newBook);
-    displayBook(newBook);
-    dialogBox.close();
+    if (bookArray[0] || bookArray[1] || bookArray[2]) {
+        const newBook = new Book(
+            bookArray[0],
+            bookArray[1],
+            bookArray[2],
+            bookArray[3]
+        );
+        addBookToLibrary(newBook);
+        displayBook(newBook);
+        dialogBox.close();
+    } else {
+        dialogBox.close();
+    }
 });
 
-function Book(title, author, genre) {
+function Book(title, author, genre, read) {
     this.title = title;
     this.author = author;
     this.genre = genre;
+    this.read = read;
 }
+
+Book.prototype.changeReadStatus = function () {
+    if (this.read === "No") {
+        this.read = "Yes";
+    } else {
+        this.read = "No";
+    }
+};
 
 function addBookToLibrary(newBook) {
     return myLibrary.push(newBook);
 }
 
-/* addBookToLibrary("The Hobbit", "Tolkien", "Fantasy");
-addBookToLibrary("Dune", "Herbert", "SF");
-addBookToLibrary("Ranma 1/2", "Takahashi", "Manga");
- */
+addBookToLibrary(bookOne);
+addBookToLibrary(bookTwo);
+addBookToLibrary(bookThree);
 
 function displayBook(book) {
     const bookCards = document.createElement("div");
@@ -56,17 +77,45 @@ function displayBook(book) {
     const genre = document.createElement("p");
     genre.className = "bookInfos";
 
+    const readStatus = document.createElement("p");
+    readStatus.className = "bookInfos";
+
+    const changeStatusBtn = document.createElement("button");
+    changeStatusBtn.className = "changeBtn";
+
+    changeStatusBtn.addEventListener("click", () => {
+        book.changeReadStatus();
+        readStatus.innerHTML = "<span>Read : " + book.read + "</span>";
+        readStatus.appendChild(changeStatusBtn);
+    });
+
     title.textContent = "Title : " + book.title;
     author.textContent = "Author : " + book.author;
     genre.textContent = "Genre : " + book.genre;
+    changeStatusBtn.textContent = "Change Status";
+    readStatus.innerHTML = "<span>Read : " + book.read + "</span>";
 
     bookCards.appendChild(closeCards);
     bookCards.appendChild(title);
     bookCards.appendChild(author);
     bookCards.appendChild(genre);
+    bookCards.appendChild(readStatus);
     booksContainer.appendChild(bookCards);
+    readStatus.appendChild(changeStatusBtn);
 
     closeCards.addEventListener("click", () => {
         bookCards.remove();
     });
 }
+
+function loadingLibrary() {
+    if (myLibrary.length > 0) {
+        myLibrary.forEach((book) => {
+            displayBook(book);
+        });
+    } else {
+        return;
+    }
+}
+
+loadingLibrary();
