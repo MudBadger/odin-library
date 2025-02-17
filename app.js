f52a991440c9c4b11b0fb1f25611bdf7d7caa90b
Gaëtan Charolle <gaetan.charolle@gmail.com>
2025-02-18 00:03:25 +0100

    ajout bar de recherche

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 const booksContainer = document.querySelector("#booksContainer");
 const addBookBtn = document.querySelector("#addBookBtn");
 const dialogBox = document.querySelector("#dialogBox");
@@ -6,13 +6,16 @@ const closeModalBtn = document.querySelector("#closeModalBtn");
 const submitBookBtn = document.querySelector("#submitBookBtn");
 const bookForm = document.querySelector("#bookForm");
 const dialogContainer = document.querySelector("#dialogContainer");
+const searchInput = document.querySelector("#searchInput");
 let displayedLibrary = [];
+let searchValue;
+let removeBook = false;
 
-const bookOne = new Book("The Hobbit", "Tolkien", "Fantasy", "Oui");
-const bookTwo = new Book("Dune", "Herbert", "SF", "Non");
-const bookThree = new Book("Ranma 1/2", "Takahashi", "Manga", "Oui");
+const bookOne = new Book("The Hobbit", "Tolkien", "Fantasy", "Oui", "1tol");
+const bookTwo = new Book("Dune", "Herbert", "SF", "Non", "2her");
+const bookThree = new Book("Ranma 1/2", "Takahashi", "Manga", "Oui", "3tak");
 
-//fermer et ouvrir la dialog box
+//Fermer et ouvrir la dialog box
 addBookBtn.addEventListener("click", () => {
     dialogBox.showModal();
 });
@@ -21,17 +24,24 @@ closeModalBtn.addEventListener("click", () => {
 });
 dialogBox.addEventListener("click", () => dialogBox.close());
 dialogContainer.addEventListener("click", (event) => event.stopPropagation());
+
 //Soumettre nouveau livre
 submitBookBtn.addEventListener("click", (event) => {
     event.preventDefault();
     const bookInputs = [...bookForm.elements];
     const bookArray = bookInputs.map((input) => input.value);
-    if (bookArray[0] || bookArray[1] || bookArray[2]) {
+    const generatedId = `${Date.now()}_${Math.random()
+        .toString(36)
+        .substring(2, 7)}`;
+
+    bookArray.push(generatedId);
+    if (bookArray) {
         const newBook = new Book(
             bookArray[0],
             bookArray[1],
             bookArray[2],
-            bookArray[3]
+            bookArray[3],
+            bookArray[4]
         );
         addBookToLibrary(newBook);
         loadingLibrary();
@@ -40,21 +50,20 @@ submitBookBtn.addEventListener("click", (event) => {
         dialogBox.close();
     }
 });
+
 //Prototype objet livre
-function Book(title, author, genre, read) {
+function Book(title, author, genre, read, bookId) {
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.read = read;
+    this.bookId = bookId;
 }
 
 Book.prototype.changeReadStatus = function () {
-    if (this.read === "Non") {
-        this.read = "Oui";
-    } else {
-        this.read = "Non";
-    }
+    this.read === "Oui" ? (this.read = "Non") : (this.read = "Oui");
 };
+
 //Ajouter et afficher des livres dans la bibliothèque
 function addBookToLibrary(newBook) {
     return myLibrary.push(newBook);
@@ -63,14 +72,25 @@ function addBookToLibrary(newBook) {
 addBookToLibrary(bookOne);
 addBookToLibrary(bookTwo);
 addBookToLibrary(bookThree);
-
-function displayBook(book) {
+function displayBook(book, removeBook) {
     const bookCards = document.createElement("div");
     bookCards.className = "bookCards";
-
+    bookCards.id = book.bookId;
+    if (removeBook) {
+        document.getElementById(book.bookId).remove();
+        return;
+    }
     const closeCards = document.createElement("div");
     closeCards.className = "closeCards";
     closeCards.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
+
+    closeCards.addEventListener("click", () => {
+        bookCards.remove();
+        myLibrary = myLibrary.filter((object) => object.bookId !== book.bookId);
+        /*  booksContainer.innerHTML = "";
+        booksContainer.append(addBookBtn);
+        loadingLibrary(); */
+    });
 
     const title = document.createElement("p");
     title.className = "bookInfos";
@@ -103,10 +123,6 @@ function displayBook(book) {
     booksContainer.append(bookCards);
     document.querySelector("#addBookBtn").after(bookCards);
     readStatus.append(changeStatusBtn);
-
-    closeCards.addEventListener("click", () => {
-        bookCards.remove();
-    });
 }
 
 function loadingLibrary() {
@@ -128,4 +144,30 @@ function loadingLibrary() {
     }
 }
 
+//rechercher dans la barre de recherche
+function searchLibrary() {
+    searchInput.addEventListener("input", (event) => {
+        const searchValue = event.target.value.toLowerCase();
+        booksContainer.innerHTML = "";
+        booksContainer.append(addBookBtn);
+
+        if (searchValue === "") {
+            loadingLibrary();
+        } else {
+            myLibrary.forEach((book) => {
+                const matchesSearch = [
+                    book.title,
+                    book.author,
+                    book.genre,
+                ].some((field) => field.toLowerCase().includes(searchValue));
+
+                if (matchesSearch) {
+                    displayBook(book);
+                }
+            });
+        }
+    });
+}
+
 loadingLibrary();
+searchLibrary();
